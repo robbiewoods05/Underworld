c7909d584430874884ed72c9824ae77444268b54
Vivi <Vivi@mailinator.com>
2012-05-14 23:53:39 -0700

    Forgot to add main.h

---
+++ Underworld/main.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_native_dialog.h>
+#include <allegro5/allegro_primitives.h>
+
+#define WIDTH 800
+#define HEIGHT 480
+#define FPS 60
